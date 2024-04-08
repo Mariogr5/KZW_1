d0d6e70fdb1fd25495d728d9a655ac8b67de6cd6
Mateusz Klimza <mateusz.klimza@idealogic.pl>
2024-04-08 18:14:24 +0200

    update

--- C_test.cpp
+++ C_test.cpp
@@ -41,10 +41,7 @@ TxtReader read_data(string file_path)
 		}
 
 		file_tasks.push_back(Task(a, b, c));
-
-		//cout << a <<" "<< b << " " << c << endl;
 		iterator++;
-
 	}
 	
 	return TxtReader(1, iterator, file_tasks);
@@ -71,12 +68,6 @@ void sort_vector(vector<Task> &myVector, int n)
 void sort_by_preparation(TxtReader &datas)
 {
 	sort_vector(datas._tasks, datas._counter);
-	
-	/*cout << endl;
-	for (int i = 0; i < datas._counter; i++)
-	{
-		cout << datas._tasks[i].prepare << " " << datas._tasks[i].work << " " << datas._tasks[i].end << endl;
-	}*/
 }
 
 
@@ -97,7 +88,6 @@ int get_time(TxtReader &datas)
 
 	vector<Task> tasks = datas._tasks;
 	vector<int> end_times;
-	//show_vector(tasks);
 	for (int i = 0; i < datas._counter; i++)
 	{
 		if (counter < tasks[i].prepare)
@@ -105,20 +95,17 @@ int get_time(TxtReader &datas)
 			counter = tasks[i].prepare;
 		}
 		counter += tasks[i].work;
-		//cout << "Counter : " << counter << "  ";
 		end_times.push_back(counter + tasks[i].end);
-		//cout << end_times[i] << endl;
 	}
 	int max_time = max_of_list(end_times, datas._counter);
 
-	//cout << "\nMAX TIME: " << max_time << endl;
+	cout << "\nMAX TIME: " << max_time << endl;
 	return max_time;
 }
 
 
 vector<Task> sollution_1(TxtReader& datas) 
 {
-	// tasks must be sorted by R 
 	vector<Task> avaliabeTask;
 	vector <Task> order;
 
@@ -155,53 +142,44 @@ vector<Task> sollution_1(TxtReader& datas)
 	return order;
 }
 
-void sollution_2(TxtReader& datas)
-{
-	sort_by_preparation(datas);
-	vector<Task> tasks = datas._tasks;
-
-	srand(time(NULL));
-	int randomIterator;
-	int counter = 4000000;
-	int randomSwapIterator;
-	int otoczenie = 4;
-	vector<Task> tasksCopy;
-	int temp = 1000000;
-	for (int i = 0; i <= counter; i +=1 )
-	{
-		randomIterator = rand() % 24;
-		randomSwapIterator = rand() % otoczenie + (randomIterator - otoczenie/2);
-		//cout << endl << randomSwapIterator << "   " << randomIterator;
-		if (randomSwapIterator > 23)
-			randomSwapIterator = 23;
-		if (randomSwapIterator < 0)
-			randomSwapIterator = 0;
-		swap(tasks[randomIterator], tasks[randomSwapIterator]);
-		datas._tasks = tasks;
-		int time = get_time(datas);
-		if(time < temp)
-		{
-			temp = time;
-			tasksCopy = tasks;
-		}
-	}
-
-	cout << "\n\nBEST TIMEEEEEEE: " << temp << endl;
-
-}
-
 int main()
 {
 	TxtReader reader2 = read_data("zadanie4.txt");
-	//sort_by_preparation(reader2);
-	//reader2._tasks = sollution_1(reader2);
-	sollution_2(reader2);
-	//show_vector(reader2._tasks);
-	//get_time(reader2);
+	sort_by_preparation(reader2);
+	reader2._tasks = sollution_1(reader2);
+	//sollution_2(reader2);
+	show_vector(reader2._tasks);
+	get_time(reader2);
 }
 
-//Możliwe rozwiązania:
-//Broadforce
-//Tabu Search
-//pamiętamy 10 ostatnich rozwiązań i szukamy najlepszy, otoczenie, pomijając rozwiązania już sprawdzone
-//Znajdujemy rozwiązania po r i szukamy optymalnego 
+
+//void sollution_2(TxtReader& datas)
+//{
+//	sort_by_preparation(datas);
+//	vector<Task> tasks = datas._tasks;
+//	srand(time(NULL));
+//	int randomIterator;
+//	int counter = 4000000;
+//	int randomSwapIterator;
+//	int otoczenie = 4;
+//	vector<Task> tasksCopy;
+//	int temp = 1000000;
+//	for (int i = 0; i <= counter; i +=1 )
+//	{
+//		randomIterator = rand() % 24;
+//		randomSwapIterator = rand() % otoczenie + (randomIterator - otoczenie/2);
+//		if (randomSwapIterator > 23)
+//			randomSwapIterator = 23;
+//		if (randomSwapIterator < 0)
+//			randomSwapIterator = 0;
+//		swap(tasks[randomIterator], tasks[randomSwapIterator]);
+//		datas._tasks = tasks;
+//		int time = get_time(datas);
+//		if(time < temp)
+//		{
+//			temp = time;
+//			tasksCopy = tasks;
+//		}
+//	}
+//	cout << "\n\nBEST TIME: " << temp << endl;
+//}
